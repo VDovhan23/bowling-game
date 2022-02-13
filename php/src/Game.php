7fec8e2f881113fefa6d766758220a4e0acdc87d
VDovhan23 <maximus23>
2022-02-13 15:17:41 +0200

    no SOLID version

--- php/src/Game.php
+++ php/src/Game.php
@@ -4,17 +4,73 @@ namespace swkberlin;
 
 class Game
 {
-    const GAME_FRAMES = 10;
+    const FRAMES = 10;
 
     protected array $rolls = [];
 
-    public function roll(int $pins): void
+    public function roll(int $skittles): void
     {
-        $this->rolls[] = $pins;
+        $this->rolls[] = $skittles;
     }
+
 
     public function score()
     {
+        $score = 0;
+        $roll = 0;
 
+        foreach (range(1, self::FRAMES) as $frame){
+            if ($this->isStrikeRoll($roll)){
+                $score += $this->throwCount($roll) + $this->strikeBonus($roll);
+
+                $roll += 1;
+
+                continue;
+            }
+
+            $score += $this->calculateFrameScore($roll);
+
+            if ($this->isStrikeRoll($roll)) {
+                $score += $this->spareBonus($roll);
+            }
+
+            $roll+= 2;
+
+        }
+
+        return $score;
     }
+
+    protected function throwCount(int $roll): int
+    {
+        return $this->rolls[$roll];
+    }
+
+    protected function calculateFrameScore(int $roll): int
+    {
+        return $this->throwCount($roll) + $this->throwCount($roll + 1);
+    }
+
+
+    protected function isStrikeRoll(int $roll): bool
+    {
+        return $this->throwCount($roll) === 10;
+    }
+
+    protected function isSpareRoll(int $roll): bool
+    {
+        return $this->calculateFrameScore($roll) === 10;
+    }
+
+
+    protected function spareBonus(int $roll): int
+    {
+        return $this->throwCount($roll + 2);
+    }
+
+    protected function strikeBonus(int $roll): int
+    {
+        return $this->throwCount($roll + 1) + $this->throwCount($roll + 2);
+    }
+
 }
